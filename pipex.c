d289611695cf392992a2a97c4ec3caab3348927c
aran <laran89@naver.com>
2023-11-07 20:59:01 +0100

    pipex.h

--- pipex.c
+++ pipex.c
@@ -3,38 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   pipex.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: arlee <arlee@student.42.fr>                +#+  +:+       +#+        */
+/*   By: aaaaaran <aaaaaran@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/24 18:47:00 by arlee             #+#    #+#             */
-/*   Updated: 2023/11/02 19:42:57 by arlee            ###   ########.fr       */
+/*   Updated: 2023/11/07 20:58:36 by aaaaaran         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
-#include <unistd.h>
-#include <sys/types.h>
-
-int main() 
-{
-    pid_t pid;
-    int x;
-    x = 0;
-
-    pid = fork();
-    if(pid > 0)
-    {  // 부모 코드
-        x = 1;
-        printf("부모 PID : %ld,  x : %d , pid : %d\n",(long)getpid(), x, pid);
-    }
-    else if(pid == 0)
-    {  // 자식 코드
-        x = 2;
-        printf("자식 PID : %ld,  x : %d\n",(long)getpid(), x);
-    }
-    else
-    {  // fork 실패
-        printf("fork Fail! \n");
-        return -1;
-    }
-    return 0;
-}
--- pipex.h
+++ pipex.h
@@ -3,10 +3,26 @@
 /*                                                        :::      ::::::::   */
 /*   pipex.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: arlee <arlee@student.42.fr>                +#+  +:+       +#+        */
+/*   By: aaaaaran <aaaaaran@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/24 18:47:03 by arlee             #+#    #+#             */
-/*   Updated: 2023/10/24 18:55:58 by arlee            ###   ########.fr       */
+/*   Updated: 2023/11/07 20:58:15 by aaaaaran         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef PIPEX_H
+#define PIPEX_H
+
+#include <unistd.h>
+#include <stdio.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include "libft/libft.h"
+
+void child_process(char **argv, char **env, int *fd);
+void parent_process(char **argv, char **env, int *fd);
+void free_cmd(char **cmd);
+char *find_path(char *cmd, char **env);
+void ft_execve(char *argv, char **env);
+
+#endif
